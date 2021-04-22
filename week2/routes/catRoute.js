07f8e7cc596da6cef060437a5b3afbeccc678e50
DDiyar <ddogru10@gmail.com>
2021-04-22 23:45:29 +0300

    grouping routers

--- week2/routes/catRoute.js
+++ week2/routes/catRoute.js
@@ -2,25 +2,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  console.log('get cat');
-  res.send('Hello cat!');
+router.route('/')
+  .get((req, res) => {
+    console.log('get all cats');
+    res.send('Hello cat!');
+})
+  .post((req, res) => {
+    console.log('post cat');
+    res.send('post cat');
 });
 
-router.post('/', (req, res) => {
-  console.log('post cat');
-  res.send('post cat');
+router.route('/:id')
+  .get((req, res) => {
+    console.log('get one cat by id', req.params);
+    res.send(`Hello cat with id ${req.params.id}!`);
+})
+  .put((req, res) => {
+    console.log('put cat', req.params);
+    res.send('put cat');
+})
+  .delete((req, res) => {
+    console.log('delete cat', req.params);
+    res.send('delete cat');
 });
-
-router.put('/', (req, res) => {
-  console.log('put cat');
-  res.send('put cat');
-});
-
-router.delete('/', (req, res) => {
-  console.log('delete cat');
-  res.send('delete cat');
-});
-
 module.exports = router;
 // catRoute
