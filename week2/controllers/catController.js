42e7402a0b0a3ccd6b803e9073240b5c02246395
DDiyar <ddogru10@gmail.com>
2021-05-04 22:03:56 +0300

    order by query tehty

--- week2/controllers/catController.js
+++ week2/controllers/catController.js
@@ -1,25 +1,36 @@
 'use strict';
+
 const catModel = require('../models/catModel');
+
 const cats = catModel.cats;
-const cat_list_get = (req, res) => {
+
+const cat_list_get = async (req, res) => { // need async here too
   console.log('get all the cats from controllers', req.query);
-  if(req.query.sort == 'age') {
-    const catsSort = cats.slice().sort((catA, catB) => catA.age - catB.age); // 1 line = can do without {
+  if(req.query.sort === 'age') {
+    const catsSort = await catModel.getAllCatsSort('age')
     res.json(catsSort);
     return; // force exit method
   }
+  else if (req.query.sort === 'name') {
+    const catsSort = await catModel.getAllCatsSort('name')
+    res.json(catsSort);
+    return;
+  }
+
+  const cats = await catModel.getAllCats();
   res.json(cats);
 };
 
 const cat_get_by_id = (req, res) => {
   console.log('get one cat by id', req.params);
-  res.json(cats.find(cat => cat.id == req.params.id));
+  res.json(cats.find(cat => cat.id === req.params.id));
 };
 
 const cat_post_new_cat = (req, res) => {
   console.log('post cat', req.body);
   res.send(`post cat: ${req.body.name}`);
 };
+
 module.exports = {
   cat_list_get,
   cat_get_by_id,
